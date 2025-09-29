be3899b1ee3703686cc31b605ca2863e263ebd2a
daooki <vanerpkrabi@gmail.com>
2025-09-29 13:34:50 +0200

    added move

--- Steamless/Assets/Scripts/movement.cs
+++ Steamless/Assets/Scripts/movement.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
+    public float MovementSpeed = 1f;
+    public float JumpForce = 1f;
+    private Rigidbody2D rigidbody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Time.deltaTime, 0, 0);
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(rigidbody.velocity.y) < 0.001f)
+        {
+            rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+        }        
     }
 }
